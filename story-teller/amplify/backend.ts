674c477eabe45e5cb69514d454743b83f92a8c36
Erik Hanchett <ehhanche@amazon.com>
2024-12-13 15:00:56 -0800

    Updated per code review

--- story-teller/amplify/backend.ts
+++ story-teller/amplify/backend.ts
@@ -1,10 +1,7 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import {
-  generateImage,
-  MODEL_ID as MODEL_ID_STABLE,
-} from "./functions/generateImage/resource";
+import { generateImage, MODEL_ID } from "./functions/generateImage/resource";
 import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { storage, knowledgeBaseBucket } from "./storage/resource";
 import { getNews } from "./functions/getNews/resource";
@@ -12,6 +9,7 @@ import { getNews } from "./functions/getNews/resource";
 const KB_REGION = "us-west-2";
 
 // Replace with your own Knowledge Base ID
+// https://docs.aws.amazon.com/bedrock/latest/userguide/knowledge-base-create.html
 const KB_ID = "F123456789";
 
 const backend = defineBackend({
@@ -27,7 +25,7 @@ backend.generateImage.resources.lambda.addToRolePolicy(
   new PolicyStatement({
     effect: Effect.ALLOW,
     actions: ["bedrock:InvokeModel"],
-    resources: [`arn:aws:bedrock:*::foundation-model/${MODEL_ID_STABLE}`],
+    resources: [`arn:aws:bedrock:*::foundation-model/${MODEL_ID}`],
   })
 );
 
