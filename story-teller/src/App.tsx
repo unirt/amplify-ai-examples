4e1ddb9cae54c38435f53e1393ad1fcb090cdf83
Danny Banks <djb@amazon.com>
2024-12-13 14:49:52 -0800

    minor cleanup

--- story-teller/src/App.tsx
+++ story-teller/src/App.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, View } from "@aws-amplify/ui-react";
+import {ThemeStyle} from '@aws-amplify/ui-react/server'
 import Hero from "./pages/Hero";
 import Story from "./pages/Story";
 import Chat from "./pages/Chat";
-import "./App.css";
+import "@aws-amplify/ui-react/styles/reset.css";
 import "@aws-amplify/ui-react/styles.css";
+import { theme } from "./theme";
 
 function App() {
   return (
-    <>
+    <View {...theme.containerProps()} color="font.primary">
       <Authenticator>
         <Routes>
           <Route path="/" element={<Hero />} />
@@ -16,7 +18,8 @@ function App() {
           <Route path="/story/:id" element={<Story />} />
         </Routes>
       </Authenticator>
-    </>
+      <ThemeStyle theme={theme} />
+    </View>
   );
 }
 
